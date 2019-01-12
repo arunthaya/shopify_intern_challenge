fc17cb722e1af939744d401079ab5751b6e32f1b
Arun Thayanithy <arunthaya@gmail.com>
2019-01-12 13:24:42 -0500

    Use standard to fix all .js files

--- client/src/Components/SearchResult.js
+++ client/src/Components/SearchResult.js
@@ -1,38 +1,37 @@
-import React, { Component } from 'react';
-import entities from 'entities';
-import ReactHtmlParser from 'react-html-parser';
-import '../App.css';
+import React, { Component } from 'react'
+import entities from 'entities'
+import ReactHtmlParser from 'react-html-parser'
+import '../App.css'
 
 class SearchResult extends Component {
-	
-	constructor(props){
-		super(props);
-		this.handleStarClick = this.handleStarClick.bind(this);
-	}
+  constructor (props) {
+    super(props)
+    this.handleStarClick = this.handleStarClick.bind(this)
+  }
 
-	handleStarClick(val){
-		//console.log(val);
-		this.props.handleClick(val);
-	}
+  handleStarClick (val) {
+    // console.log(val);
+    this.props.handleClick(val)
+  }
 
-	render(){
-		return(
-			<React.Fragment>
-    		<tr>
-		      <td 
-		      	onClick={() => this.handleStarClick(this.props.id)} 
-		      	className="Star"
-		      	id={this.props.favourited ? "highlighted" : null}
+  render () {
+    return (
+      <React.Fragment>
+        <tr>
+          <td
+            onClick={() => this.handleStarClick(this.props.id)}
+            className='Star'
+            id={this.props.favourited ? 'highlighted' : null}
 		      	>&#9733;
-	      	</td>
-		      <td>{this.props.title}</td>
-		      <td>{ReactHtmlParser(entities.decodeHTML(this.props.textToDisplay))}</td>
-		    </tr>
-  		</React.Fragment>
-		);
-	}
+          </td>
+          <td>{this.props.title}</td>
+          <td>{ReactHtmlParser(entities.decodeHTML(this.props.textToDisplay))}</td>
+        </tr>
+      </React.Fragment>
+    )
+  }
 }
 
-export default SearchResult;
+export default SearchResult
 
-/* onClick={() => this.handleStarClick(this.props.id)}*/
+/* onClick={() => this.handleStarClick(this.props.id)} */
--- client/src/utilities/StringHelper.js
+++ client/src/utilities/StringHelper.js
@@ -1,28 +1,25 @@
 const helpers = {
-  isEmpty: function(str){
-    if(!str){
-      return true;
-    }
-    else if(str.length === 0){
-      return true;
-    }
-    else if(/^\s*$/.test(str)){
-      return true;
-    }
-    else if(!str.trim()){
-      return true;
+  isEmpty: function (str) {
+    if (!str) {
+      return true
+    } else if (str.length === 0) {
+      return true
+    } else if (/^\s*$/.test(str)) {
+      return true
+    } else if (!str.trim()) {
+      return true
     } else {
-      return false;
+      return false
     }
   },
-  isSameSearch: function(str1, str2, results){
-    if( str1.trim() === str2 && results.length != 0){
-      return;
+  isSameSearch: function (str1, str2, results) {
+    if (str1.trim() === str2 && results.length != 0) {
+      return
     }
-    if(helpers.isEmpty(str1)){
-      return;
+    if (helpers.isEmpty(str1)) {
+
     }
   }
 }
 
-export default helpers;
+export default helpers
